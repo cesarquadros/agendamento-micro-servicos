9462c414758c4356d9140090d6b942c1c47548c0
Cesar Quadros <cesar.quadros@b2wdigital.com>
2019-05-20 13:15:00 -0300

    inclusao d o Zuul e Eureka

--- salas-agendamento-agenda/src/main/java/br/com/salasagendamento/SalasAgendamentoAgendamentoApplication.java
+++ salas-agendamento-agenda/src/main/java/br/com/salasagendamento/SalasAgendamentoAgendamentoApplication.java
@@ -2,10 +2,12 @@ package br.com.salasagendamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"br.com.salasagendamento"})
+@EnableEurekaClient
 public class SalasAgendamentoAgendamentoApplication {
 
 	public static void main(String[] args) {
