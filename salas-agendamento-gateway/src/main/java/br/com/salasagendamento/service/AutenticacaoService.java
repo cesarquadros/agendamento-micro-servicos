1b4e71d45e4866984c82dd0665292f57e492bc2d
Cesar <cesar.quadros@b2wdigital.com>
2019-06-03 15:43:36 -0300

    alteracao relacionado a chamada do servico cliente

--- salas-agendamento-gateway/src/main/java/br/com/salasagendamento/service/AutenticacaoService.java
+++ salas-agendamento-gateway/src/main/java/br/com/salasagendamento/service/AutenticacaoService.java
@@ -1,5 +1,7 @@
 package br.com.salasagendamento.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,16 +10,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.salasagendamento.controller.AutenticacaoController;
+
 @Service
 public class AutenticacaoService {
 
 	@Value("${gateway.cliente-integration.url-user}")
 	private String URL;
 	
+	private Logger LOG = LoggerFactory.getLogger(AutenticacaoService.class);
+	
 	public Boolean autenticar(String user, String pass) {
 		RestTemplate restTemplate = new RestTemplate();
+		LOG.info(">>>>>>>>>>>>>>>>>>> Setando dados no header");
 		HttpHeaders headers = getHeader(user, pass);
 		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+		
+		LOG.info(">>>>>>>>>>>>>>>>>>> Realizando chamada do serviço cliente");
 		ResponseEntity<Boolean> forEntity = restTemplate.exchange(URL, HttpMethod.GET, entity, Boolean.class);
 		return forEntity.getBody();
 	}
