9e55799db166945c2d89d8491409e50e436efc1a
Cesar <cesar.quadros@b2wdigital.com>
2019-06-03 18:47:56 -0300

    Adicionado regra para liberar o metodo post cliente para nao autenticar

--- salas-agendamento-gateway/src/main/java/br/com/salasagendamento/filter/GatewayFilter.java
+++ salas-agendamento-gateway/src/main/java/br/com/salasagendamento/filter/GatewayFilter.java
@@ -14,17 +14,23 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
-public class GatewayFilter extends ZuulFilter{
-	
+public class GatewayFilter extends ZuulFilter {
+
 	private Logger LOG = LoggerFactory.getLogger(GatewayFilter.class);
 
 	private static final String USUARIO_NAO_AUTORIZADO = "Usuario não autorizado";
 	private static final String REALIZAR_O_LOGIN = "Realizar o LOGIN";
 	private static final String PRE = "pre";
 	private static final String TOKEN = "token";
+
+	private static final String SERVICO_CLIENTE = "cliente";
+
+	private static final String METHOD_POST = "POST";
+	
+	
 	@Autowired
 	private RedisTemplate<String, String> redisTemplate;
-	
+
 	@Override
 	public boolean shouldFilter() {
 		return true;
@@ -32,40 +38,58 @@ public class GatewayFilter extends ZuulFilter{
 
 	@Override
 	public Object run() throws ZuulException {
-		
+
 		LOG.info(">>>>>>>>>>>>>>>>>>>>> Acessando Zuul");
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
 		
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-        
-        LOG.info("Recuperando TOKEN do cabeçalho");
-        String headerToken = request.getHeader(TOKEN);
-        
-        LOG.info("Token recuperado do cabeçalho: {}", headerToken);
-        
-        if(ObjectUtils.isEmpty(headerToken) || !verificaToken(headerToken)) {
-        	LOG.info(">>>>>>>>>>>>>>>>>>>>>>> TOKEN INEXISTENTE OU INVALIDO");
-         	ZuulException zuulException = new ZuulException(REALIZAR_O_LOGIN, HttpStatus.UNAUTHORIZED.value(), USUARIO_NAO_AUTORIZADO);
-        	throw new ZuulRuntimeException(zuulException);
-        }
+		if (precisaAutenticar(request)) {
+			verificaSessao(request);
+		}
 		return true;
 	}
-	
+
+	public Boolean precisaAutenticar(HttpServletRequest request) {
+		StringBuffer urlBuffer = request.getRequestURL();
+		String url[] = urlBuffer.toString().split("/");
+		String servicoSolicitado = url[url.length - 1];
+		String method = request.getMethod();
+
+		if (servicoSolicitado.equalsIgnoreCase(SERVICO_CLIENTE) && method.equalsIgnoreCase(METHOD_POST)) {
+			return false;
+		}
+		return true;
+	}
+
+	public void verificaSessao(HttpServletRequest request) {
+		LOG.info("Recuperando TOKEN do cabeçalho");
+		String headerToken = request.getHeader(TOKEN);
+		LOG.info("Token recuperado do cabeçalho: {}", headerToken);
+
+		if (ObjectUtils.isEmpty(headerToken) || !verificaToken(headerToken)) {
+			LOG.info(">>>>>>>>>>>>>>>>>>>>>>> TOKEN INEXISTENTE OU INVALIDO");
+			ZuulException zuulException = new ZuulException(REALIZAR_O_LOGIN, HttpStatus.UNAUTHORIZED.value(),
+					USUARIO_NAO_AUTORIZADO);
+			throw new ZuulRuntimeException(zuulException);
+		}
+	}
+
 	public Boolean verificaToken(String token) {
 		LOG.info("Verificando se TOKEN é valido");
 		String tokenExistente = redisTemplate.opsForValue().get(token);
-		if(ObjectUtils.isEmpty(tokenExistente)) {
+		if (ObjectUtils.isEmpty(tokenExistente)) {
 			LOG.info("TOKEN não é valido");
 			return false;
 		}
 		LOG.info("TOKEN é valido");
 		return true;
 	}
-	
+
 	@Override
 	public String filterType() {
 		return PRE;
 	}
+
 	@Override
 	public int filterOrder() {
 		return 1;
